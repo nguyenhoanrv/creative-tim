f095ff5c2c3e5993ad4f1bf230e1841cc7b35b11
NGUYEN VAN HOAN 20184106 <hoan.nv184106@sis.hust.edu.vn>
2020-03-19 19:16:40 +0700

    xong

---
+++ 1.js
@@ -0,0 +1,44 @@
+﻿function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+const img = document.getElementsByClassName("img");
+
+function scale() {
+    let x = getRandomInt(img.length);
+    img[x].classList.add("imgAnimation");
+    setTimeout(() => {
+        img[x].classList.remove("imgAnimation");
+    }, 1000);
+}
+setInterval(scale, 2000);
+
+var nav = document.getElementById('navbar');
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    nav.classList.add('navbar1');
+} else {
+    nav.classList.remove('navbar1');
+}
+
+const slider = document.querySelector('.list-logo');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX); //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
